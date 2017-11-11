5d2391e299870f270555f6d0efb7192fab8d7b46
Kamal Sharif <ksharifbd@gmail.com>
2017-11-11 18:43:36 +0600

    added postcss config

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+var autoprefixer = require('autoprefixer');
+
+module.exports = {
+    plugins: [
+        autoprefixer(
+            {
+               browsers: ["last 10 versions", "not OperaMini > 0", "not UCAndroid > 0", "not QQAndroid > 0"]
+            }
+        )
+    ]
+}
