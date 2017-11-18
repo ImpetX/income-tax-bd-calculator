9aae5aa167a14bbf79bbe5640446822acc31a66b
Kamal Sharif <ksharifbd@gmail.com>
2017-11-18 22:44:39 +0600

    fixed console warning for SelectBlock props

--- src/data/Months.js
+++ src/data/Months.js
@@ -6,62 +6,62 @@ const Months = [
 
     {
         value: '1',
-        label: 1
+        label: '1'
     },
 
     {
         value: '2',
-        label: 2
+        label: '2'
     },
 
     {
         value: '3',
-        label: 3
+        label: '3'
     },
 
     {
         value: '4',
-        label: 4
+        label: '4'
     },
 
     {
         value: '5',
-        label: 5
+        label: '5'
     },
 
     {
         value: '6',
-        label: 6
+        label: '6'
     },
 
     {
         value: '7',
-        label: 7
+        label: '7'
     },
 
     {
         value: '8',
-        label: 8
+        label: '8'
     },
 
     {
         value: '9',
-        label: 9
+        label: '9'
     },
 
     {
         value: '10',
-        label: 10
+        label: '10'
     },
 
     {
         value: '11',
-        label: 11
+        label: '11'
     },
 
     {
         value: '12',
-        label: 12
+        label: '12'
     }
 ];
 
