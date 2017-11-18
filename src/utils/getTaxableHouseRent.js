c13d14c99425fc147e4a34273e82e989c343b18c
Kamal Sharif <ksharifbd@gmail.com>
2017-11-18 22:51:53 +0600

    added comments

--- src/utils/getTaxableHouseRent.js
+++ src/utils/getTaxableHouseRent.js
@@ -1,7 +1,7 @@
 import getLowerValue from './getLowerValue';
 
 function getTaxableHouseRent(houseRent, basicSalary) {
-    const THRESHOLD_HOUSE_RENT = 25000;
+    const THRESHOLD_HOUSE_RENT = 25000; // monthly
     let halfOfBasicSalary = basicSalary/2;
     let deductibleAmount = getLowerValue(THRESHOLD_HOUSE_RENT, halfOfBasicSalary);
 
