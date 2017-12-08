374be4d86875cccf45d2fe4eb9ce3edc25b760ee
Kamal Sharif <ksharifbd@gmail.com>
2017-12-08 11:51:24 +0600

    added test for filterInput

---
+++ src/utils/__tests__/filterInput.spec.js
@@ -0,0 +1,11 @@
+import filterInput from '../filterInput';
+
+describe('filterInput', () => {
+    it('Should output "250000" when input is "250,000"', () => {
+        expect(filterInput('250,000')).toBe('250000');
+    });
+
+    it('Should output "250000" when input is "2,50,000"', () => {
+        expect(filterInput('2,50,000')).toBe('250000');
+    });
+});
