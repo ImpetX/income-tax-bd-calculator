ab228c60a1193cf6b049c894439da8741d1a0318
Kamal Sharif <ksharifbd@gmail.com>
2017-12-09 16:52:59 +0600

    "added more tests to filterInput"

--- src/utils/__tests__/filterInput.spec.js
+++ src/utils/__tests__/filterInput.spec.js
@@ -8,4 +8,8 @@ describe('filterInput', () => {
     it('Should output "250000" when input is "2,50,000"', () => {
         expect(filterInput('2,50,000')).toBe('250000');
     });
+
+    it('Should output "" when input is ""', () => {
+        expect(filterInput('')).toBe('');
+    });
 });
