d113697538221f768ed80f9dcb2abeb5ff47feb0
Kamal Sharif <ksharifbd@gmail.com>
2017-12-10 16:54:09 +0600

    added test data

---
+++ src/data/Test.js
@@ -0,0 +1,32 @@
+const Test = {
+    salary: {
+        basic: 50000,
+        houseRent: 25000,
+        medicalAllowance: 5000,
+        conveyanceAllowance: 20000
+    },
+
+    bonus: 100000,
+
+    taxable: {
+        basic: 600000,
+        houseRent: 0,
+        medicalAllowance: 0,
+        conveyanceAllowance: 210000,
+        bonus: 100000,
+        total: 910000
+    },
+
+    taxaLiability: 79000,
+
+    investment: {
+        allowance: 227500,
+        made: 220000
+    },
+
+    taxRebate: 33000,
+
+    netTax: 46000
+};
+
+export default Test;
