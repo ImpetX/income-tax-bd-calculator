bd86d8fc1d60e185fbce7a0a82e48e8deca8c2e3
Kamal Sharif <ksharifbd@gmail.com>
2017-12-10 22:07:44 +0600

    fixed logical bugs

--- src/utils/getTaxableHouseRent.js
+++ src/utils/getTaxableHouseRent.js
@@ -4,11 +4,11 @@ import multiplyByMonths from './multiplyByMonths';
 function getTaxableHouseRent(houseRent, thresholdHouseRent, basicSalary, percentage, months) {
     let gross = {
         houseRent: multiplyByMonths(houseRent, months),
-        thresholdHouseRent: multiplyByMonths(thresholdHouseRent, months),
-        basicSalary: multiplyByMonths(basicSalary, months)
+        thresholdHouseRent: thresholdHouseRent * 12,
+        basicSalary: (basicSalary * percentage) * 12
     };
 
-    let deductibleAmount = getLowerValue(gross.thresholdHouseRent, (gross.basicSalary)/2);
+    let deductibleAmount = getLowerValue(gross.thresholdHouseRent, gross.basicSalary);
 
     return gross.houseRent > deductibleAmount ? gross.houseRent - deductibleAmount : 0;
 }
